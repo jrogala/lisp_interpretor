340a4479e493d16de6036a8cf8018efdcaee9151
Jimmy Rogala <jimmy@studio17>
2016-04-25 00:20:50 +0200

    added error catching in setq

--- Code/toplevel.cc
+++ Code/toplevel.cc
@@ -8,25 +8,31 @@
 extern "C" FILE *yyin;
 
 bool is_setq(Object l){
-  if (!listp(l)) return false;
-  if (null(l)) return false;
-  if (!(car(l)->is_string() || car(l)->is_symbol())) return false;
-  if (Object_to_string(car(l)) != "setq") return false;
-  if (null(cdr(l))) return false; // renvoyer des erreurs pour rattraper les erreurs et informer l'utilisateur des erreurs qu'il a codé en faisant des erreurs.
-  if (null(cddr(l))) return false;
-  if (null(cdddr(l))) return true;
-  return false;
+  if (!listp(l)) throw 1;
+  if (null(l)) throw 1;
+  if (!(car(l)->is_string() || car(l)->is_symbol())) throw 1;
+  if (Object_to_string(car(l)) != "setq") throw 1;
+  if (null(cdr(l))) throw 2; // renvoyer des erreurs pour rattraper les erreurs et informer l'utilisateur des erreurs qu'il a codé en faisant des erreurs.
+  if (null(cddr(l))) throw 2;
+  if (null(cdddr(l))) throw 0;
+  throw 3;
 }
 
 void toplevel(){
   Environment env;
   do {
     Object l = read_obj();
-
-    if (is_setq(l)) {
-      env.add_new_binding(Object_to_string(cadr(l)),eval(caddr(l),env));
-      cout << "SET" << endl;
-      continue;
+    try {is_setq(l);}
+			catch (int e){
+				switch (e) {
+				case 1: break;
+				case 2: cout << "too short" << endl;continue;break;
+				case 3: cout << "too long" << endl;continue;break;
+				case 0:
+				  env.add_new_binding(Object_to_string(cadr(l)),eval(caddr(l),env));
+				  cout << "SET" << endl;
+				  continue;break;
+				}
     }
     cout << eval(l,env) << endl;
     } while (!feof(yyin));
